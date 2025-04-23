f7e2601eda7f953e5a612ed9d27ab9b79910f875
Jayson P <jnp1380@rit.edu>
2025-04-23 04:19:57 -0400

    Some updates to map

--- src/map.js
+++ src/map.js
@@ -1,97 +1,108 @@
 import * as utils from "./utils.js";
 
+let poiPage = "";
 const drawRoute = () => {
-    let startingLocation;
-    let start;
-    let end;
-    let route;
-    let zoom;
-    let userCoords = {};
-    const fromTo = document.querySelector("#fromTo");
+  let startingLocation;
+  let start;
+  let end;
+  let route;
+  let zoom;
+  let userCoords = {};
+  const fromTo = document.querySelector("#fromTo");
 
-    startingLocation = "notStarted"; // ************** WILL NEED TO CHANGE THIS TO BE DYNAMIC
+  startingLocation = "notStarted"; // ************** WILL NEED TO CHANGE THIS TO BE DYNAMIC
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                userCoords.lat = position.coords.latitude;
-                userCoords.lon = position.coords.longitude;
-                // console.log(position);
-                console.log("LAT: ", userCoords.lat);
-                console.log("LON: ", userCoords.lon);
-                if (startingLocation == "notStarted") {
-                    route = [userCoords, utils.poiLocations[0]];
-                    start = "Your location";
-                    end = utils.poiLocations[0].name;
-                    zoom = 11;
-                } else if (startingLocation == "xerox") {
-                    route = [utils.poiLocations[0], utils.poiLocations[1]];
-                    start = utils.poiLocations[0].name;
-                    end = utils.poiLocations[1].name;
-                    zoom = 17;
-                } else if (startingLocation == "bausch") {
-                    route = [utils.poiLocations[1], utils.poiLocations[2]];
-                    start = utils.poiLocations[1].name;
-                    end = utils.poiLocations[2].name;
-                    zoom = 15;
-                } else if (startingLocation == "kodak") {
-                    route = [utils.poiLocations[2], utils.poiLocations[3]];
-                    start = utils.poiLocations[2].name;
-                    end = utils.poiLocations[3].name;
-                    zoom = 15;
-                } else if (startingLocation == "itx") {
-                    route = [utils.poiLocations[3], utils.poiLocations[4]];
-                    start = utils.poiLocations[3].name;
-                    end = utils.poiLocations[4].name;
-                    zoom = 15;
-                } else if (startingLocation == "millerQuad") {
-                    route = [utils.poiLocations[4], utils.poiLocations[5]];
-                    start = utils.poiLocations[4].name;
-                    end = utils.poiLocations[5].name;
-                    zoom = 15;
-                } else if (startingLocation == "envative") {
-                    route = [utils.poiLocations[5], utils.poiLocations[6]];
-                    start = utils.poiLocations[5].name;
-                    end = utils.poiLocations[6].name;
-                    zoom = 16;
-                } else if (startingLocation == "artCenterQuad") {
-                    route = [utils.poiLocations[6], utils.poiLocations[7]];
-                    start = utils.poiLocations[6].name;
-                    end = utils.poiLocations[7].name;
-                    zoom = 16;
-                }
-            
-                fromTo.textContent = `${start} -> ${end}`;
-            
-            
-                
-            
-            
-                utils.setupMap(route, zoom, userCoords);
-            
-                //redraw map every 10 sec to show movement of user
-                setTimeout(drawRoute, 10000);
-            },
-            (error) => {
-                console.error("Error getting location:", error);
-            }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        userCoords.lat = position.coords.latitude;
+        userCoords.lon = position.coords.longitude;
+        // console.log(position);
+        console.log(
+          "nearest" + utils.isLocationClose(userCoords, utils.poiLocations[0])
         );
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
+        console.log("LAT: ", userCoords.lat);
+        console.log("LON: ", userCoords.lon);
+        poiPage = "";
+        if (startingLocation == "notStarted") {
+          route = [userCoords, utils.poiLocations[0]];
+          start = "Your location";
+          end = utils.poiLocations[0].name;
+          zoom = 11;
+          if (utils.isLocationClose(userCoords, utils.poiLocations[0])) {
+            poiPage = "../poiLocations/Xerox.html";
+          }
+        } else if (startingLocation == "xerox") {
+          route = [utils.poiLocations[0], utils.poiLocations[1]];
+          start = utils.poiLocations[0].name;
+          end = utils.poiLocations[1].name;
+          zoom = 17;
+          if (utils.isLocationClose(userCoords, utils.poiLocations[1])) {
+            poiPage = "../poiLocations/Baush.html";
+          }
+        } else if (startingLocation == "bausch") {
+          route = [utils.poiLocations[1], utils.poiLocations[2]];
+          start = utils.poiLocations[1].name;
+          end = utils.poiLocations[2].name;
+          zoom = 15;
+          if (utils.isLocationClose(userCoords, utils.poiLocations[0])) {
+            poiPage = "../poiLocations/Kodak.html";
+          }
+        } else if (startingLocation == "kodak") {
+          route = [utils.poiLocations[2], utils.poiLocations[3]];
+          start = utils.poiLocations[2].name;
+          end = utils.poiLocations[3].name;
+          if (utils.isLocationClose(userCoords, utils.poiLocations[0])) {
+            poiPage = "../poiLocations/Itx.html";
+          }
+          zoom = 15;
+        } else if (startingLocation == "itx") {
+          route = [utils.poiLocations[3], utils.poiLocations[4]];
+          start = utils.poiLocations[3].name;
+          end = utils.poiLocations[4].name;
+          zoom = 15;
+        } else if (startingLocation == "millerQuad") {
+          route = [utils.poiLocations[4], utils.poiLocations[5]];
+          start = utils.poiLocations[4].name;
+          end = utils.poiLocations[5].name;
+          zoom = 15;
+        } else if (startingLocation == "envative") {
+          route = [utils.poiLocations[5], utils.poiLocations[6]];
+          start = utils.poiLocations[5].name;
+          end = utils.poiLocations[6].name;
+          zoom = 16;
+        } else if (startingLocation == "artCenterQuad") {
+          route = [utils.poiLocations[6], utils.poiLocations[7]];
+          start = utils.poiLocations[6].name;
+          end = utils.poiLocations[7].name;
+          zoom = 16;
+        }
 
-    console.log("USER COORDS: ", userCoords);
+        fromTo.textContent = `${start} -> ${end}`;
 
-    
-}
+        utils.setupMap(route, zoom, userCoords);
+
+        //redraw map every 10 sec to show movement of user
+        setTimeout(drawRoute, 10000);
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+
+  console.log("USER COORDS: ", userCoords);
+};
 
 const arriveBtn = document.querySelector("#arrive-btn");
-arriveBtn.addEventListener('onclick', () => {
-    
-})
+arriveBtn.addEventListener("onclick", () => {
+  window.location.href = poiPage;
+});
 
 const init = () => {
-    drawRoute();
-}
+  drawRoute();
+};
 
 window.onload = init;
